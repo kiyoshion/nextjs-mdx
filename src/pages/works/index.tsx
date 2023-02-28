0f3731f53c808e30fb6f31d26586e5f3eecc1a01
kiyoshion <kiyoshion@gmail.com>
2023-02-28 23:22:23 +0900

    add works

---
+++ src/pages/works/index.tsx
@@ -0,0 +1,61 @@
+import Layout from 'components/Layout';
+import fs from 'fs';
+import matter from 'gray-matter';
+import Link from 'next/link';
+
+type WorkProps = {
+  id: string;
+  meta: {
+    title: string;
+    desc: string;
+    launchedAt: string;
+  };
+}
+
+type WorksProps = {
+  works: WorkProps[];
+}
+
+export const getStaticProps = () => {
+  const files = fs.readdirSync('src/pages/works/data');
+  const works = files.map((fileName) => {
+    const id = fileName.replace(/\.mdx$/, '');
+    const fileContent = fs.readFileSync(`src/pages/works/data/${fileName}`, 'utf-8');
+    const { data } = matter(fileContent);
+    console.log(data)
+
+    return {
+      id,
+      meta: data,
+    };
+  });
+
+  return {
+    props: {
+      works,
+    },
+  };
+};
+
+export default function Works({ works }: WorksProps) {
+  return (
+    <Layout
+      meta={{
+        title: 'Works',
+        desc: '制作実績'
+      }}
+    >
+      <div className='flex flex-col'>
+        <h1>Works</h1>
+        <p>What I did, What I wanted to.</p>
+      </div>
+      {works.map((work: WorkProps) => (
+        <Link href={`/works/${work.id}`} key={work.id}>
+          <h2>{work.meta.title}</h2>
+        </Link>
+      ))}
+    </Layout>
+  )
+}
+
+
