989f3c5372fc8a0710c15ff9f47b36b18b8f0e0a
kiyoshion <kiyoshion@gmail.com>
2023-03-04 20:33:31 +0900

    update me

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -6,8 +6,10 @@ export default function Me() {
       title: 'About',
       desc: 'About',
     }}>
-      <h1>About</h1>
-      <p>This is about</p>
+      <div className='max-w-7xl mx-auto p-4'>
+        <h1>About</h1>
+        <p>This is about</p>
+      </div>
     </Layout>
   );
 }
